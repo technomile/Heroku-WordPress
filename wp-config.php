889dad03ca29769a1ac9de48d44e7393003c9db1
Inderjeet Sohal <inderjeets@technomile.com>
2015-03-11 19:11:52 +0530

    Updating ClearDB

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,6 @@
 <?php
 $my_env_var = getenv('CLEARDB_DATABASE_URL');
 
-if(!empty($my_env_var)){
-
 $cleardb_explode = explode("mysql://",$my_env_var);
 $cleardb_username_explode = explode(":",$cleardb_explode[1]);
 $cleardb_password_explode = explode("@",$cleardb_username_explode[1]);
@@ -63,14 +61,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '/neJfY_Kd3[K2dC8A:!8{KI&xoUF;@^Ea=g<w}v{&PhdprEy|+BEir<:.:Jz{Mf?');
+define('SECURE_AUTH_KEY',  'q=2IcOGU5XE&mk]!b=d$A{I(]3Yp*qt&BPsqPn4eyL)f22I5}c<A% 6L<0cN:-V|');
+define('LOGGED_IN_KEY',    'gb7R|5nX}8^z[H%,^5Ne~mc(A,{~)Af>Nvy8UDk$4+JV8gc!kA<QfrhMkCq7WQ[o');
+define('NONCE_KEY',        ' f*3i?Po=@kA*Z--/#d_t?Di}5RPKM2|y^#sX!KJOfIbI2U=XlxCOWRU|L8N^:Y/');
+define('AUTH_SALT',        'Gx&$8f,!`#KU*;bhOwSKu@y|$Ij:V%4ebHENu4PNERwicFzr+BLsd8])0si0$C]4');
+define('SECURE_AUTH_SALT', 'Y4UnD%-Zb/EpsX;&j1-KK]X1E:)/9O@[qU<23x;QqdBIi;,=%H>j]Ro80BrP VZ~');
+define('LOGGED_IN_SALT',   '[s=EoN|ZQ04n+j&fDLxV1%g(X&{R4cfj];%4}]=<R^aLt)?sA<j?Fw6kBtP!0B9K');
+define('NONCE_SALT',       'k cck)IwS[F!m}>2`n)!$V^qOo=cA|dXN_KY3E7QO@|U:xgzT awq Q!/8 ^UnU(');
 
 /**#@-*/
 
@@ -80,7 +78,17 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'tm_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
 
 /**
  * For developers: WordPress debugging mode.
@@ -99,8 +107,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-}else{
-	echo 'Please install ClearDB and after installing reload this page to proceed ';?><a target="_blank" href="https://addons.heroku.com/cleardb?utm_campaign=category&utm_medium=dashboard&utm_source=addons#ignite">Install ClearDB Ignite</a>
-	<?php	
-}
-
