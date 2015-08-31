f09b8606ed25ba7194029ecd9041506bdd263c0a
Greg Nokes <tsykoduk@gmail.com>
2015-08-31 11:46:08 -0700

    adding in S3 support via heroku config variables

--- wp-config.php
+++ wp-config.php
@@ -17,6 +17,11 @@
  *
  * @package WordPress
  */
+
+// S3 Config Info - read the S3 Access Keys from the config //
+define( ‘AWS_ACCESS_KEY_ID’, $_ENV[“AWS_ACCESS_KEY_ID”]);
+define( ‘AWS_SECRET_ACCESS_KEY’, $_ENV[“AWS_SECRET_ACCESS_KEY”]);
+
  
 // ** Heroku Postgres settings - from Heroku Environment ** //
 $db = parse_url($_ENV["CLEARDB_DATABASE_URL"]); 
